113293a90d9d249450fc06f483a672c8e6a8a66d
Jim Pelletier <jim@Jims-MacBook-Pro.local>
2019-11-07 18:27:15 +1100

    adds oh-my-zsh config

--- config_bootstrap.sh
+++ config_bootstrap.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
